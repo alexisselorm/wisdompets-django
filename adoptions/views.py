a248367e832cc91af69e79e781319ba6fe4195cf
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-11-25 00:15:30 +0200

    URL patterns

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pet
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<p>Home View</p>')
+
+def pet_detail(request,pet_id):
+    return HttpResponse(f'<p>pet detail view with id {pet_id}</p>')
